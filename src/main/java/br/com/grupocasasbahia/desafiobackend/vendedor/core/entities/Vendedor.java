60dc4889529f3deae45bcaa489e7a00447db2fa9
Jonathan de Souza <Jonathan.desouza@viavarejo.com.br>
2025-06-20 17:11:16 -0300

    feature: Validação de documento (CNPJ e CPF), e ID Filial

--- src/main/java/br/com/grupocasasbahia/desafiobackend/vendedor/core/entities/Vendedor.java
+++ src/main/java/br/com/grupocasasbahia/desafiobackend/vendedor/core/entities/Vendedor.java
@@ -1,13 +1,9 @@
 package br.com.grupocasasbahia.desafiobackend.vendedor.core.entities;
 
 import br.com.grupocasasbahia.desafiobackend.vendedor.core.enums.TipoDeContratacao;
-import org.springframework.cglib.core.Local;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.Date;
 import java.util.UUID;
 import java.util.regex.Pattern;
 
@@ -49,6 +45,31 @@ public class Vendedor {
             return false;
         }
 
+        if(!validaDocumentoEmBrancoOuVazio()){
+            error = "Documento é obrigatório";
+            return false;
+        }
+
+        if(!validaDocumentoTodosDigitosIguais(limparDocumento(documento))){
+            error = "Documento inválido, todos dígitos são iguais";
+            return false;
+        }
+
+        if(tipoDeContratacao != TipoDeContratacao.PJ && !validaCpf()){
+            error = "CPF inválido";
+            return false;
+        }
+
+        if(tipoDeContratacao == TipoDeContratacao.PJ && !validaCnpj()){
+            error = "CNPJ inválido";
+            return false;
+        }
+
+        if(!validaIdFilial()){
+            error = "Id filial deve ser maior que 0";
+            return false;
+        }
+
         return true;
     }
 
@@ -57,13 +78,6 @@ public class Vendedor {
             return false;
 
         return Pattern.matches("^([A-Za-zÀ-ÿ]+\\s)+[A-Za-zÀ-ÿ]+$", nome);
-    }
-
-    private Boolean validaEmail(){
-        if(email == null || email.trim().isEmpty())
-            return false;
-
-        return Pattern.matches("^(.+)\\@(.+)$", email);
     }
 
     private Boolean validaDataNascimento(){
@@ -77,6 +91,133 @@ public class Vendedor {
         return true;
     }
 
+    private Boolean validaDocumentoEmBrancoOuVazio(){
+        Integer TamanhoValido = 11;
+
+        if(documento == null || documento.trim().isEmpty())
+            return false;
+
+        return true;
+    }
+
+    private Boolean validaDocumentoTodosDigitosIguais(String documento){
+        Character primeiroDigito = documento.charAt(0);
+        Boolean todosOsDigitosSaoIguais = true;
+
+        for(Character digito : documento.toCharArray()){
+            if(digito != primeiroDigito)
+                todosOsDigitosSaoIguais = false;
+        }
+
+        if(todosOsDigitosSaoIguais)
+            return false;
+
+        return true;
+    }
+
+    private static String limparDocumento(String documento){
+        return documento.replaceAll("\\D", "");
+    }
+
+    private Boolean validaCpf(){
+
+        Integer tamanhoCpfValido = 11;
+        String cpf = limparDocumento(documento);
+        Integer tamanhoCpf = cpf.length();
+
+        if(tipoDeContratacao != TipoDeContratacao.PJ && tamanhoCpf != tamanhoCpfValido)
+            return false;
+
+        return aplicarRegrasDeDigitosCPF(cpf);
+    }
+
+    private Boolean aplicarRegrasDeDigitosCPF(String cpf){
+        // Referencia https://www.macoratti.net/alg_cpf.htm#:~:text=O%20algoritmo%20de%20valida%C3%A7%C3%A3o%20do,%3A%20111.444.777%2D05.
+
+        Integer primeiroDigito = calculaDigitoCPF(cpf,10);
+        Integer segundoDigito = calculaDigitoCPF(cpf,11);
+        String digitosCalculados = primeiroDigito.toString() + segundoDigito.toString();
+        String digitosOriginais = cpf.substring(cpf.length() -2);
+
+        if(!digitosCalculados.equals(digitosOriginais))
+            return false;
+
+        return true;
+    }
+
+    private Integer calculaDigitoCPF(String cpf, Integer fator){
+        Integer total = 0;
+
+        for (Character digito : cpf.toCharArray()){
+            if (fator > 1)
+                total += Integer.parseInt(digito.toString()) * fator --;
+        }
+
+        Integer resto = total % 11;
+
+        return resto < 2 ? 0 : 11 - resto;
+    }
+
+    private Boolean validaCnpj(){
+
+        Integer tamanhoCnpjValido = 14;
+        String cnpj = limparDocumento(documento);
+        Integer tamanhoCnpj = cnpj.length();
+
+        if(tipoDeContratacao != TipoDeContratacao.PJ && tamanhoCnpj != tamanhoCnpjValido)
+            return false;
+
+        return aplicarRegrasDeDigitosCnpj(cnpj);
+    }
+
+    private Boolean aplicarRegrasDeDigitosCnpj(String cnpj){
+        // https://www.macoratti.net/alg_cnpj.htm#:~:text=O%20n%C3%BAmero%20que%20comp%C3%B5e%20o,que%20s%C3%A3o%20os%20d%C3%ADgitos%20verificadores.
+
+        Integer primeiroDigito = calculaDigitoCNPJ(cnpj,12);
+        Integer segundoDigito = calculaDigitoCNPJ(cnpj,13);
+        String digitosCalculados  = primeiroDigito.toString() + segundoDigito.toString();
+        String digitosOriginais  = cnpj.substring(cnpj.length() -2);
+
+        if(!digitosCalculados.equals(digitosOriginais))
+            return false;
+
+        return true;
+    }
+
+    private Integer calculaDigitoCNPJ(String cnpj, Integer posicao){
+        int[] pesos;
+
+        if(posicao == 12){
+            pesos = new int[]{5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+        }
+        else if (posicao == 13){
+            pesos = new int[]{6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+        }
+        else{
+            throw new IllegalArgumentException("Posição inválida para cálculo do dígito do CNPJ");
+        }
+
+        int total = 0;
+        for (int index = 0; index < pesos.length; index ++){
+            total += Character.getNumericValue(cnpj.charAt(index)) * pesos[index];
+        }
+
+        int resto = total % 11;
+
+        return resto < 2 ? 0 : 11 - resto;
+    }
+
+    private Boolean validaEmail(){
+        if(email == null || email.trim().isEmpty())
+            return false;
+
+        return Pattern.matches("^(.+)\\@(.+)$", email);
+    }
+
+    private Boolean validaIdFilial(){
+
+        return idFilial > 0;
+    }
 
     public UUID getId() {
         return id;
--- src/test/java/br/com/grupocasasbahia/desafiobackend/vendedor/unit/VendedorTest.java
+++ src/test/java/br/com/grupocasasbahia/desafiobackend/vendedor/unit/VendedorTest.java
@@ -43,7 +43,7 @@ public class VendedorTest {
         String cnpj = "33041260094711";
         String email = "john@gmail.com";
 
-        Vendedor vendedor = new Vendedor(matricula,nome,dataNascimento,cnpj,email, TipoDeContratacao.CLT,1);
+        Vendedor vendedor = new Vendedor(matricula,nome,dataNascimento,cnpj,email, TipoDeContratacao.PJ,1);
 
         assertTrue(vendedor.vendedorEhValido());
         assertNotNull(vendedor.getId());
@@ -98,5 +98,81 @@ public class VendedorTest {
 
         assertFalse(vendedor.vendedorEhValido());
         assertEquals("Idade inválida, vendedor precisa ter 16 anos completos", vendedor.getError());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    public void NaoDeveCriarUmVendedorDocumentoVazio(String cpf){
+        //arrange
+        String matricula ="2345211-CLT";
+        String nome = "Jonathan de Souza";
+        String email = "john@gmail.com";
+        LocalDate dataNascimento = LocalDate.parse("1997-01-24");
+
+        Vendedor vendedor = new Vendedor(matricula,nome,dataNascimento,cpf,email, TipoDeContratacao.CLT,1);
+
+        assertFalse(vendedor.vendedorEhValido());
+        assertEquals("Documento é obrigatório", vendedor.getError());
+    }
+
+    @Test
+    public void NaoDeveCriarUmVendedorDocumentoComTodosDigitosIguais(){
+        //arrange
+        String matricula ="2345211-CLT";
+        String nome = "Jonathan de Souza";
+        String email = "john@gmail.com";
+        String cpf = "11111111111";
+        LocalDate dataNascimento = LocalDate.parse("1997-01-24");
+
+        Vendedor vendedor = new Vendedor(matricula,nome,dataNascimento,cpf,email, TipoDeContratacao.CLT,1);
+
+        assertFalse(vendedor.vendedorEhValido());
+        assertEquals("Documento inválido, todos dígitos são iguais", vendedor.getError());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "123", "71428793806", "87748248810", "877.482.488-70", "877.482.48850", "877.48248888"})
+    public void NaoDeveCriarUmVendedorCPFInvalido(String cpf){
+        //arrange
+        String matricula ="2345211-CLT";
+        String nome = "Jonathan de Souza";
+        String email = "john@gmail.com";
+        LocalDate dataNascimento = LocalDate.parse("1997-01-24");
+
+        Vendedor vendedor = new Vendedor(matricula,nome,dataNascimento,cpf,email, TipoDeContratacao.CLT,1);
+
+        assertFalse(vendedor.vendedorEhValido());
+        assertEquals("CPF inválido", vendedor.getError());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "45.723.174/0001-15", "03918103000153", "1232", "05.587.148/0001-05", "14.300.716/0001-78", "60742648000171"})
+    public void NaoDeveCriarUmVendedorCNPJInvalido(String cnpj){
+        //arrange
+        String matricula ="2345211-CLT";
+        String nome = "Jonathan de Souza";
+        String email = "john@gmail.com";
+        LocalDate dataNascimento = LocalDate.parse("1997-01-24");
+
+        Vendedor vendedor = new Vendedor(matricula,nome,dataNascimento,cnpj,email, TipoDeContratacao.CLT,1);
+
+        assertFalse(vendedor.vendedorEhValido());
+        assertEquals("CPF inválido", vendedor.getError());
+    }
+
+    @Test
+    public void NaoDeveCriarUmVendedorIDFilialIgualAZero(){
+
+        //arrange
+        String matricula ="2345211-CLT";
+        String nome = "Jonathan de Souza";
+        LocalDate dataNascimento = LocalDate.parse("1997-01-24");
+        String cpf = "42184073806";
+        String email = "john@gmail.com";
+
+        Vendedor vendedor = new Vendedor(matricula,nome,dataNascimento,cpf,email, TipoDeContratacao.CLT,0);
+
+        assertFalse(vendedor.vendedorEhValido());
+        assertEquals("Id filial deve ser maior que 0", vendedor.getError());
     }
 }
